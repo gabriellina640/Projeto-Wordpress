d305f25e18be471ceb3dae335b95d9a13372f0a4
gabriellina640 <gabriellina640@gmail.com>
2025-08-04 09:55:36 -0500

    Adiciona scripts de backup e git push

--- backup_db.sh
+++ backup_db.sh
@@ -3,9 +3,9 @@
 # Container do banco
 DB_CONTAINER="wordpress-docker_db_1"
 
-# Usuário e senha do banco (coloque suas credenciais reais)
+# Usuário e senha do banco (root e senha conforme seu docker-compose)
 DB_USER="root"
-DB_PASS="1234"
+DB_PASS="rootpass"
 
 # Pasta para salvar backups localmente
 BACKUP_DIR="./backups"
@@ -17,6 +17,6 @@ mkdir -p "$BACKUP_DIR"
 BACKUP_FILE="$BACKUP_DIR/backup_$(date +%Y%m%d_%H%M%S).sql"
 
 # Faz dump do banco dentro do container e salva localmente
-docker exec $DB_CONTAINER sh -c "exec mysqldump -u$DB_USER -p'$DB_PASS' --all-databases" > "$BACKUP_FILE"
+docker exec $DB_CONTAINER sh -c "exec mysqldump -u$DB_USER -p$DB_PASS --all-databases" > "$BACKUP_FILE"
 
 echo "Backup criado em: $BACKUP_FILE"
