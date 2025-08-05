929844bb10695a4d3866537c19abace0ecbac1d1
gabriellina640 <gabriellina640@gmail.com>
2025-08-05 13:06:22 -0500

    atualizacoes

---
+++ wp-content/languages/loco/plugins/woocommerce-pt_BR.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'WooCommerce 10.0.4','report-msgid-bugs-to'=>'https://wordpress.org/support/plugin/woocommerce','last-translator'=>'','language-team'=>'Português do Brasil','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2025-07-23T08:28:13+00:00','po-revision-date'=>'2025-08-05 16:23+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'woocommerce
+','language'=>'pt_BR','plural-forms'=>'nplurals=2; plural=n != 1;','x-loco-version'=>'2.8.0; wp-6.8.2; php-8.2.29','messages'=>[]];
